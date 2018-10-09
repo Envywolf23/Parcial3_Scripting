cf9e8ac03a50eddb6c44ac43e29fbafda860a295
Envywolf23 <pedrojuan133@hotmail.com>
2018-10-09 07:07:01 -0500

    Assets

---
+++ Scripting_201820_Parcial3_Base-supletorio/Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float damagePts = 1F;
+
+    [SerializeField]
+    private float destroyTime = 3F;
+
+    [SerializeField]
+    private float launchForce = 100F;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * launchForce, ForceMode.Impulse);
+        Invoke("DestroyBullet", destroyTime);
+    }
+
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ActorController actor = other.GetComponent<ActorController>();
+
+        if (actor != null)
+        {
+            actor.onDamageTaken(damagePts);
+        }
+    }
+}
---
+++ Scripting_201820_Parcial3_Base-supletorio/Assets/Scripts/Controllers/ActorController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Collider))]
+public abstract class ActorController : MonoBehaviour
+{
+    protected NavMeshAgent agent;
+
+    [SerializeField]
+    private Bullet bulletGO;
+
+    [SerializeField]
+    private float healthPoints = 100F;
+
+    [SerializeField]
+    private float spawnOffset = 1.5F;
+
+    public delegate void OnDamageTaken(float damagePts);
+
+    public OnDamageTaken onDamageTaken;
+
+    public float CurrentHP { get; protected set; }
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        CurrentHP = healthPoints;
+
+        onDamageTaken += ApplyDamage;
+    }
+
+    private void ApplyDamage(float damagePts)
+    {
+        CurrentHP -= damagePts;
+
+        if (CurrentHP <= 0F)
+        {
+        }
+    }
+
+    protected abstract Vector3 GetTargetLocation();
+
+    protected void Shoot()
+    {
+        if (bulletGO != null)
+        {
+            Instantiate<Bullet>(bulletGO, transform.position + new Vector3(0F, 0F, spawnOffset), transform.rotation);
+        }
+    }
+
+    protected void MoveActor()
+    {
+        agent.SetDestination(GetTargetLocation());
+    }
+
+    protected virtual void OnDestroy()
+    {
+        agent = null;
+    }
+}
